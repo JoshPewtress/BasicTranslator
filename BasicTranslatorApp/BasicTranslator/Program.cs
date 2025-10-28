fb2ab6a2f1d5085ffe570f797b57c448558aeadf
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-28 16:36:57 -0600

    feat: add console UI with language selection

--- BasicTranslatorApp/BasicTranslator/Program.cs
+++ BasicTranslatorApp/BasicTranslator/Program.cs
@@ -1,3 +1,55 @@
-﻿using System.Text;
+﻿using BasicTranslator;
 
-Console.ReadLine();
+do
+{
+	SelectDictionary();
+}
+while (ConfirmContinue());
+
+void SelectDictionary()
+{
+	FrenchDictionary fd = new();
+	SpanishDictionary sd = new();
+
+	Console.Write(
+		$"""
+		Select a language to translate too.
+		1 - French
+		2 - Spanish
+		Make selection: 
+		""");
+	var selection = Console.ReadLine() ?? "";
+
+	switch (selection)
+	{
+		case "1":
+			fd.Translate(UserInput());
+			break;
+        case "2":
+            sd.Translate(UserInput());
+            break;
+        default:
+            Console.WriteLine($"\"{selection}\" was not a valid selection.");
+			break;
+	}
+}
+
+string UserInput()
+{
+	Console.Write("Enter a sentence to translate: ");
+	return Console.ReadLine() ?? "";
+}
+
+bool ConfirmContinue()
+{
+	Console.WriteLine();
+    Console.Write("Translate again (Y\\N): ");
+	var response = Console.ReadLine();
+
+    if (response.Trim().ToUpper() == "Y")
+    {
+		return true;
+    }
+
+	return false;
+}
