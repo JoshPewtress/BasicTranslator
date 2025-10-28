ad3799e91be1458a7947bca22f6ee71c6fbe5496
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-28 11:55:11 -0600

    feat: add Spanish Dictionary with 36 common words

--- BasicTranslatorApp/BasicTranslator/Program.cs
+++ BasicTranslatorApp/BasicTranslator/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+public static class SpanishDictionary
+{
+    private static readonly Dictionary<string, string> _SpanishDictionary = new()
+    {
+        {"I", "Yo"},
+        {"You", "Tú"},
+        {"He", "Él"},
+        {"She", "Ella"},
+        {"We", "Nosotros"},
+        {"They", "Ellos"},
+        {"Friend", "Amigo"},
+        {"Family", "Familia"},
+        {"House", "Casa"},
+        {"Car", "Coche"},
+        {"Book", "Libro"},
+        {"Table", "Mesa"},
+        {"Chair", "Silla"},
+        {"Door", "Puerta"},
+        {"Window", "Ventana"},
+        {"Phone", "Teléfono"},
+        {"Day", "Día"},
+        {"Night", "Noche"},
+        {"Morning", "Mañana"},
+        {"Week", "Semana"},
+        {"Year", "Año"},
+        {"School", "Escuela"},
+        {"Work", "Trabajo"},
+        {"Sun", "Sol"},
+        {"Moon", "Luna"},
+        {"Water", "Agua"},
+        {"Fire", "Fuego"},
+        {"Tree", "Árbol"},
+        {"Flower", "Flor"},
+        {"Be", "Estar"},
+        {"Have", "Tener"},
+        {"Go", "Ir"},
+        {"See", "Ver"},
+        {"Eat", "Comer"},
+        {"Speak", "Hablar"},
+        {"Live", "Vivir"},
+        {"Like", "Gustar"}
+    };
+
+    
+}
